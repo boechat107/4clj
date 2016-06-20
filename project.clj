730bcb92a585b1549ea7897b3abd86dac7b7f45f
Andre Ambrosio Boechat <boechat107@gmail.com>
2016-06-20 16:49:36 +0200

    Minor changes on project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:plugins [[venantius/ultra "0.4.1"]]}}
-  :min-lein-version "2.5.2")
+  :dependencies [[org.clojure/clojure "1.8.0"]])
